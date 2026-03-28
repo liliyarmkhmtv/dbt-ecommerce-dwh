a2986858b4db04be87f51366557997a4b7664f6e
liliyarmkhmtv <liliyarmkhmtv@gmail.com>
2026-03-28 19:09:32 +0300

    refactor: add script for loading all raw tables

--- scripts/load_raw_data.py
+++ scripts/load_raw_data.py
@@ -1,28 +1,36 @@
 from google.cloud import bigquery
 
-# Construct a BigQuery client object.
-client = bigquery.Client(project="dbt-ecommerce-dwh-491416")
+project = "dbt-ecommerce-dwh-491416"
+tables = {'ecommerce.customers': 'data/olist_customers_dataset.csv',
+          'ecommerce.order_items': 'data/olist_order_items_dataset.csv',
+          'ecommerce.order_reviews': 'data/olist_order_reviews_dataset.csv',
+          'ecommerce.orders': 'data/olist_orders_dataset.csv',
+          'ecommerce.products': 'data/olist_products_dataset.csv',
+          'ecommerce.sellers': 'data/olist_sellers_dataset.csv',
+          'ecommerce.product_category_name_translation': 'data/product_category_name_translation.csv',
+          'geo.geolocation': 'data/olist_geolocation_dataset.csv',
+          'payments.order_payments': 'data/olist_order_payments_dataset.csv'}
 
-# Set table_id to the ID of the table to create.
-table_id = "dbt-ecommerce-dwh-491416.ecommerce.olist_customers"
+client = bigquery.Client(project=project)
 
 job_config = bigquery.LoadJobConfig(
     source_format=bigquery.SourceFormat.CSV,
     skip_leading_rows=1,
     autodetect=True,
     write_disposition="WRITE_TRUNCATE",
+    allow_quoted_newlines=True,
+    ignore_unknown_values=True
 )
 
-file_path = "data/olist_customers_dataset.csv"
+for table_name, file_path in tables.items():
+    with open(file_path, "rb") as source_file:
+        job = client.load_table_from_file(source_file, f"{project}.{table_name}", job_config=job_config)
 
-with open(file_path, "rb") as source_file:
-    job = client.load_table_from_file(source_file, table_id, job_config=job_config)
+        job.result()  # Waits for the job to complete.
 
-job.result()  # Waits for the job to complete.
-
-table = client.get_table(table_id)  # Make an API request.
-print(
-    "Loaded {} rows and {} columns to {}".format(
-        table.num_rows, len(table.schema), table_id
-    )
-)
+        table = client.get_table(f"{project}.{table_name}")  # Make an API request.
+        print(
+            "Loaded {} rows and {} columns to {}".format(
+                table.num_rows, len(table.schema), f"{project}.{table_name}"
+            )
+        )
